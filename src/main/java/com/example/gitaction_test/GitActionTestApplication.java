ec1534396194918eeb0c33ada269c131b5514d71
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-09-16 14:59:52 +0900

    feat: gitAtions Test All File Upload

---
+++ src/main/java/com/example/gitaction_test/GitActionTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitaction_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitActionTestApplication.class, args);
+    }
+
+}
